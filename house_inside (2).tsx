1e5393379e84ecc2b9ca8390ad5aa4f222ad362c
Robzilla888 <60526348+Robzilla888@users.noreply.github.com>
2021-01-26 20:19:01 -0500

    Add files via upload

---
+++ house_inside (2).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="house_inside (2)" tilewidth="32" tileheight="32" tilecount="224" columns="16">
+ <image source="house_inside (2).png" width="512" height="448"/>
+</tileset>
